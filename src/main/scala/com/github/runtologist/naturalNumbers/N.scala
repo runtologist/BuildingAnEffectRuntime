0b1513fc3a46d681c1d0a64a00e23b733faec055
Simon Schenk <simon@schenk-online.net>
2019-12-30 22:12:30 +0100

    stringify N to base10

--- src/main/scala/com/github/runtologist/naturalNumbers/N.scala
+++ src/main/scala/com/github/runtologist/naturalNumbers/N.scala
@@ -2,11 +2,14 @@ package com.github.runtologist.naturalNumbers
 
 import scala.annotation.tailrec
 
-sealed trait N
+sealed trait N {
+  override def toString(): String = N.stringify(this)
+}
 
 object N {
 
   case object Zero extends N
+
   case class Cons(n: N) extends N
 
   @tailrec
@@ -15,6 +18,15 @@ object N {
       case (Zero, Zero)         => true
       case (Cons(nn), Cons(mm)) => equals(nn, mm)
       case _                    => false
+    }
+
+  // convenience
+
+  @tailrec
+  def stringify(n: N, accu: Int = 0): String =
+    n match {
+      case Zero    => accu.toString()
+      case Cons(n) => stringify(n, accu + 1)
     }
 
 }
